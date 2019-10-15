2235bcb0f50b454bdf06dd3fadc105c787e4bdb5
Guilherme Andrade <guilherme.andrade@redes.unb.br>
2019-10-15 16:47:36 -0300

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'rasa_middleware_connector',         # How you named your package folder (MyLib)
   packages = ['rasa_middleware_connector'],   # Chose the same as "name"
-  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Adds middleware support for rasa connectors',   # Give a short description about your library
   author = 'Guilherme Guy',                   # Type in your name
